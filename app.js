ac66e8b1221a6ef0cf126c53f40069f9aea88e36
AlexSalam <vornat2@gmail.com>
2016-04-23 17:15:05 +0100

    Setup for basic connect/disconnect events

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-
+var clients = {};
 
 server.listen(80);
 
@@ -12,3 +12,36 @@ app.get('/', function(req, res) {
     console.log('Root request made');
 });
 
+io.on('connection', function(socket) {
+
+	socket.on('connectionAttemptEvent', function(data) {
+
+		//Function forchecking server connectivity
+
+		socket.emit('connectedEvent', {connected: true});
+	});
+
+	socket.on('disconnect', function() {
+
+		// Function for handling disconnects
+
+		socket.emit('clientDisconnectedEvent', {connected: false});
+	});
+
+	socket.on('loginEvent', function(data) {
+
+		// Function for handling session logins
+		// Accepts a data object containing a summoner name and region - also if the login is guest or real
+		if (data.guest) {
+			var $name = 'nigger';
+		}
+
+	});
+
+	socket.on('matchForGameEvent', function(data) {
+
+		// Function for finding a random match from other users in the queue
+
+	});
+
+})
