b423e25f26ddbba515111713dc995c358e0d94ac
Thomas Freeborough <b2019162@my.shu.ac.uk>
2016-04-24 01:17:34 +0100

    Added login request socket message, in addition to adding a sick rumble library

--- app/components/Main/Header/loginForm.js
+++ app/components/Main/Header/loginForm.js
@@ -5,40 +5,71 @@ var LoginForm = React.createClass({
     componentDidMount: function(){
         $(document).ready(function() {
             $('select').material_select();
+            $('#login-form-main').jrumble();
         });
+    },
+
+    loginRequest: function(){
+        var data = $('#login-form-main').serializeArray();
+        var validated = false;
+        var counter = 0;
+
+        var requestObject = {};
+
+        for(var key in data){
+            if(!data.hasOwnProperty(key)) continue;
+
+            var input = data[key];
+            console.log(input);
+            if(input["value"].length > 0){
+                counter++;
+                requestObject[input["name"]] = input["value"];
+            }else{
+                $('#login-form-main input[name="'+input["name"]+'"]').removeClass('valid').addClass('invalid');
+            }
+        }
+
+        if(counter == 3){
+            socket.emit('loginRequest',{login:requestObject});
+        }else{
+
+            $('#login-form-main').trigger('startRumble');
+            var rumbleTimeout = setTimeout(function(){$('#login-form-main').trigger('stopRumble');}, 200)
+        }
+
     },
 
     render: function(){
         return(
             <div id="login-form" className="header-secondary col s6">
-                <form>
+                <form id="login-form-main">
                     <div className="input-field col s3">
-                        <input placeholder="Summoner Name" id="summoner" type="text" className="validate" />
+                        <input placeholder="Summoner Name" name="summoner" id="summoner" type="text" className="validate" />
                             <label className="active" for="summoner">Summoner Name</label>
                     </div>
                     <div className="input-field col s2">
-                        <select>
+                        <select name="region">
                             <option value="" disabled selected>Region</option>
-                            <option value="na">NA</option>
-                            <option value="euw">EUW</option>
-                            <option value="eune">EUNE</option>
-                            <option value="jp">JP</option>
-                            <option value="kr">KR</option>
-                            <option value="lan">LAN</option>
-                            <option value="las">LAS</option>
-                            <option value="br">BR</option>
-                            <option value="oce">OCE</option>
-                            <option value="pbe">PBE</option>
-                            <option value="ru">RU</option>
-                            <option value="tr">TR</option>
+                            <option value="NA">NA</option>
+                            <option value="EUW">EUW</option>
+                            <option value="EUNE">EUNE</option>
+                            <option value="JP">JP</option>
+                            <option value="KR">KR</option>
+                            <option value="LAN">LAN</option>
+                            <option value="LAS">LAS</option>
+                            <option value="BR">BR</option>
+                            <option value="OCE">OCE</option>
+                            <option value="PBE">PBE</option>
+                            <option value="RU">RU</option>
+                            <option value="TR">TR</option>
                         </select>
                         <label>Region</label>
                     </div>
                     <div className="input-field col s3">
-                        <input placeholder="Password" id="password" type="password" className="validate" />
+                        <input placeholder="Password" name="password" id="password" type="password" className="validate" />
                         <label className="active" for="password">Password</label>
                     </div>
-                    <a className="waves-effect waves-light btn-large blue-grey login-btn">Login</a>
+                    <a className="waves-effect waves-light btn-large blue-grey login-btn" onClick={this.loginRequest}>Login</a>
                     <a className="waves-effect waves-light btn blue-grey darken-2 register-btn">Register</a>
                 </form>
             </div>
