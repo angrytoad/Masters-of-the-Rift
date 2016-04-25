e3d4996411ab8b896241b45ab7cee7b471f91fa6
AlexSalam <vornat2@gmail.com>
2016-04-26 00:37:10 +0100

    log date

--- db.js
+++ db.js
@@ -30,27 +30,29 @@ module.exports = function (mongoose) {
     
 	var $validateSession = function($token, $loginId) {
 		var $Date = new Date;
+		var $resp = null;
 		$sessions.findOne({loginId: $loginId, sessionId: $token}, 'loginId sessionId Date', function(err, session) {
 			if (err) {
 				console.log('IMPORTANT MEGAERROR: ' + err);
 			} else {
 				console.log(session);
 				if (typeof session === 'object') {
-					console.log('1');
 					if ((((new Date(session.time).getTime()) / 1000) + 2592000) < $Date.getTime() / 1000) {
-						console.log('2');
-						return {err: true, msg: 'Session exceeds 30 day timeout.'};
+						$resp = {err: true, msg: 'Session exceeds 30 day timeout.'};
 					} else {
-						console.log('3');
-						return {err: false, msg: 'Success!'};
+						$resp = {err: false, msg: 'Success!'};
 					}
-					console.log('5');
 				} else {
-					return {err: true, msg: 'No session found.'};
+					$resp = {err: true, msg: 'No session found.'};
 				}
 			}
 		});
-		return {err: true, msg: 'POO'};
+		if ($resp == null) {
+			return {err: true, msg: 'POO'};
+		} else {
+			return $resp;
+		}
+		
 	}
 	this.validateSession = $validateSession;
 }
