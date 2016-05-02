71ccfa80189209f296c4346905a1f947c80f050a
Thomas Freeborough <b2019162@my.shu.ac.uk>
2016-05-02 16:29:53 +0100

    Added riot confirmation and more match files

---
+++ app/components/Main/Game/Match/opponentInformation.js
@@ -0,0 +1,12 @@
+/** @jsx React.DOM */
+
+
+var OpponentInformation = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <h2>Opponent Information</h2>
+            </div>
+        )
+    }
+})
